654a2026cfbd2df5a7d505dcd053f29c3c3c6010
Thiago Reis <thiagoreis128@gmail.com>
2020-02-28 12:28:37 -0300

    Usando o Gatsby Node Api para criar a paginação

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,8 @@
-const { createFilePath } = require("gatsby-source-filesystem")
 const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
 
-// Add slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-
   if (node.internal.type === "MarkdownRemark") {
     const slug = createFilePath({
       node,
@@ -22,26 +20,50 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
   return graphql(`
-    {
-      allMarkdownRemark {
+        allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
-    }
-  `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    } 
+    `).then(result => {
+    const posts = result.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve("./src/templates/blog-post.js"),
-        context: {
+        component: path.resolve(`./src/templates/glog-post.js`),
+        constext: {
           slug: node.fields.slug,
+        },
+      })
+    })
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
         },
       })
     })
