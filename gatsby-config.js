8a547d476f660bc5dd11336634038fddd84466f7
Thiago Reis <thiagoreis128@gmail.com>
2020-03-10 17:12:11 -0300

    Configurando o plugin dentro do gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config()
+const queries = require("./src/utils/algolia.queries")
+
 module.exports = {
   siteMetadata: {
     title: `John Doe`,
@@ -63,6 +66,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
